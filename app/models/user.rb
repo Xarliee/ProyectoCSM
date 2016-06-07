5af74741f717e5531d95bd3a19d0da29e2817f38
Carlos <c.salas.marin@gmail.com>
2016-06-06 20:59:47 -0400

    antes de poner foto en user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   has_many :products
   has_many :craftsmen
+  before_save :default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+  before_save
   #User.invite!(:email => "new_user@platan.us")
   
 
@@ -15,7 +17,7 @@ class User < ActiveRecord::Base
  private 
   
   def default_role
-    self.role ||= 1
+    self.role ||= 0
   end
 
 end
